43b3178610b315fd3ff51dae86afdb73063fe079
Partiolainen <petersson.kiev@gmail.com>
2018-01-15 12:30:40 +0200

    dev point

--- extensions/strategies/sigcoll/strategy.js
+++ extensions/strategies/sigcoll/strategy.js
@@ -62,12 +62,14 @@ return {
     }*/
 
     //simple buy/sell for test
-    if(s.period.close!=null && s.period.sigcoll.buyprice!=null && s.period.close <= s.period.sigcoll.buyprice
-      && s.period.rsi <= s.options.oversold_rsi){
+    if(s.period.close!=null && s.period.sigcoll.buyprice!=null
+       && ((s.period.close <= s.period.sigcoll.buyprice
+       && s.period.rsi <= s.options.oversold_rsi) || s.period.sigcoll.buynow == 1)){
       s.signal = 'buy'
     }
-    else if(s.period.close!=null && s.period.sigcoll.target != null &&
-      (s.period.close >= s.period.sigcoll.target || s.period.close <= s.period.sigcoll.stoploss)){
+    else if(s.period.close!=null && ((s.period.sigcoll.target != null &&
+      (s.period.close >= s.period.sigcoll.target || s.period.close <= s.period.sigcoll.stoploss)))
+        || (s.period.sigcoll.sellnow == 1)){
       s.signal = 'sell'
     } else s.signal = 'hold'
     cb()
@@ -77,6 +79,12 @@ return {
     var cols = []
     
     if (s.period.sigcoll){
+      if (typeof s.period.sigcoll.buynow == 1) {
+        cols.push('buy now'['red'])
+      }
+      if (typeof s.period.sigcoll.sellnow == 1) {
+        cols.push('sell now'['green'])
+      }
       if (typeof s.period.sigcoll.buyprice === 'number') {
         cols.push(z(11, n(s.period.sigcoll.buyprice).format('0.00000000'), ' ')['cyan'])
       }
