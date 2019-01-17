1e71e8a8f82bc493e5807872e30b57f3d3e4637a
Partiolainen <petersson.kiev@gmail.com>
2019-01-17 23:01:37 +0200

    willr rework

--- extensions/strategies/ta_willr/strategy.js
+++ extensions/strategies/ta_willr/strategy.js
@@ -6,87 +6,62 @@ var z = require('zero-fill')
 
 module.exports = {
   name: 'ta_willr',
-  description: 'Attempts to buy low and sell high by tracking RSI high-water readings.',
+  description: 'Williams R with rsi oversold',
 
   getOptions: function () {
-    this.option('period', 'period length, same as --period_length', String, '2m')
-    this.option('period_length', 'period length, same as --period', String, '2m')
+    this.option('period', 'period length eg 5m', String, '5m')
     this.option('min_periods', 'min. number of history periods', Number, 52)
-    this.option('rsi_periods', 'number of RSI periods', Number, 14)
-    this.option('oversold_rsi', 'buy when RSI reaches or drops below this value', Number, 30)
-    this.option('overbought_rsi', 'sell when RSI reaches or goes above this value', Number, 82)
-    this.option('rsi_recover', 'allow RSI to recover this many points before buying', Number, 3)
-    this.option('rsi_drop', 'allow RSI to fall this many points before selling', Number, 0)
-    this.option('rsi_divisor', 'sell when RSI reaches high-water reading divided by this value', Number, 2)
-
+    
     this.option('willr_periods', 'number of WillR periods', Number, 14)
     this.option('willr_oversold', 'number of WillR periods', Number, -94)
-    this.option('willr_overbought', 'number of WillR periods', Number, -30)    
+    this.option('willr_overbought', 'number of WillR periods', Number, -30)
+
+    this.option('overbought_rsi_periods', 'number of periods for overbought RSI', Number, 25)
+    this.option('overbought_rsi', 'sold when RSI exceeds this value', Number, 75)
   },
 
   calculate: function (s) {
-    rsi(s, 'rsi', s.options.rsi_periods)    
+    if (s.options.overbought_rsi) {
+      // sync RSI display with overbought RSI periods
+      s.options.rsi_periods = s.options.overbought_rsi_periods
+      rsi(s, 'overbought_rsi', s.options.overbought_rsi_periods)
+      if (!s.in_preroll && s.period.overbought_rsi >= s.options.overbought_rsi && !s.overbought) {
+        s.overbought = true
+
+        if (s.options.mode === 'sim' && s.options.verbose) {
+          console.log(('\noverbought at ' + s.period.overbought_rsi + ' RSI, preparing to sold\n').cyan)
+        }
+      }
+    }
   },
 
   onPeriod: function (s, cb) {
-    if (s.in_preroll) return cb()
+    if (!s.in_preroll && typeof s.period.overbought_rsi === 'number') {
+      if (s.overbought) {
+        s.overbought = false
+        s.signal = 'sell'
+        return cb()
+      }
+    }
 
     willr(s, s.options.min_periods, s.options.willr_periods).then(function(signal) {
       s.period['willr'] = signal
-      //cb()
+
+      if (typeof s.period.willr === 'number') {
+        if(s.period.willr<=s.options.willr_oversold) {
+          s.signal = 'buy'
+        }        
+  
+        if(s.period.willr>=s.options.willr_overbought){
+          s.signal = 'sell'
+        }
+      }
+
+      cb()
     }).catch(function(error) {
       console.log(error)
       cb()
     })
-
-    if (typeof s.period.willr === 'number') {
-      if(s.period.willr<=s.options.willr_oversold) {
-        s.signal = 'buy'
-      }        
-
-      if(s.period.willr>=s.options.willr_overbought){
-        s.signal = 'sell'
-      }
-    }
-    /*if (typeof s.period.rsi === 'number') {
-      if (s.trend === undefined && s.period.rsi <= s.options.oversold_rsi) {
-        s.rsi_low = s.period.rsi
-        s.trend = 'oversold'
-      }
-      if (s.trend === 'oversold') {
-        s.rsi_low = Math.min(s.rsi_low, s.period.rsi)
-        if (s.period.rsi >= s.rsi_low + s.options.rsi_recover && 
-            s.period.willr<=s.options.willr_oversold) {
-          s.trend = 'long'
-          s.signal = 'buy'
-          s.rsi_high = s.period.rsi
-        }
-      }
-      if (s.trend !== 'oversold' && s.trend !== 'long' && s.period.rsi >= s.options.overbought_rsi) {
-        s.rsi_high = s.period.rsi
-        s.trend = 'long'
-      }
-      if (s.trend === 'long') {
-        s.rsi_high = Math.max(s.rsi_high, s.period.rsi)
-        if (s.period.rsi <= s.rsi_high / s.options.rsi_divisor) {
-          s.trend = 'short'
-          s.signal = 'sell'
-        }
-      }
-      if (s.trend === 'long' && s.period.rsi >= s.options.overbought_rsi) {
-        s.rsi_high = s.period.rsi
-        s.trend = 'overbought'
-      }
-      if (s.trend === 'overbought') {
-        s.rsi_high = Math.max(s.rsi_high, s.period.rsi)
-        if (s.period.rsi <= s.rsi_high - s.options.rsi_drop && 
-            s.period.willr>=s.options.willr_overbought) {
-          s.trend = 'short'
-          s.signal = 'sell'
-        }
-      }
-    }*/
-    cb()
   },
 
   onReport: function (s) {
@@ -98,7 +73,6 @@ module.exports = {
   },
 
   phenotypes: {
-    // -- common
     period_length: Phenotypes.ListOption(['15m','30m','1h','90m','2h','150m','3h','210m','4h']),
     min_periods: Phenotypes.Range(15, 26),
     markdown_buy_pct: Phenotypes.RangeFloat(-1, 5),
@@ -109,16 +83,11 @@ module.exports = {
     profit_stop_enable_pct: Phenotypes.Range0(1, 20),
     profit_stop_pct: Phenotypes.Range(1,20),
 
-    // -- strategy
-    //rsi_periods: Phenotypes.Range(1, 200),
-    //oversold_rsi: Phenotypes.Range(1, 100),
-    //overbought_rsi: Phenotypes.Range(1, 100),
-    //rsi_recover: Phenotypes.Range(1, 100),
-    //rsi_drop: Phenotypes.Range(0, 100),
-    //rsi_divisor: Phenotypes.Range(1, 10)
     willr_periods: Phenotypes.Range(7,25),
     willr_overbought: Phenotypes.Range(-50, 0),
-    willr_oversold: Phenotypes.Range(-100, -90)
+    willr_oversold: Phenotypes.Range(-100, -90),
+    overbought_rsi_periods: Phenotypes.Range(1, 50),
+    overbought_rsi: Phenotypes.Range(20, 100)
   }
 }
 
